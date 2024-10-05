93261c4e46934a3e143028a197d462f16f68a6f6
Nirav Parekh <niruparekh09@gmail.com>
2024-10-05 23:01:00 +0530

    feat: :heavy_plus_sign: Added Feign Client Dependency

--- booking-service/src/main/java/com/nrv/booking_service/BookingServiceApplication.java
+++ booking-service/src/main/java/com/nrv/booking_service/BookingServiceApplication.java
@@ -3,6 +3,7 @@ package com.nrv.booking_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * This is the main entry point for the Booking Service of our Microservice.
@@ -15,10 +16,11 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class BookingServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookingServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookingServiceApplication.class, args);
+    }
 
 }
